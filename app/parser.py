6932eb5dec01ad2a4a61d52fde81ada1dec8f302
Alexander Bernauer <alexander.bernauer@splintinvest.com>
2024-12-08 12:53:56 +0100

    more robust parser

--- app/parser.py
+++ app/parser.py
@@ -13,57 +13,61 @@ MAX_PACKET_LEN = 169
 def parse(input: Generator[int, None, None]) -> Generator[Packet, None, None]:
     prev_byte: int = ord("c")
     in_packet: bool = False
-    packet: list[int] = []
+    payload: Optional[list[int]] = None
+    packet: Optional[Packet] = None
+
     while True:
         cur_byte = next(input)
 
-        # If in Mode 1, enable Mode 2
-        if not in_packet and prev_byte == 224 and cur_byte == 224:
-            raise ValueError("device is in Mode 1")
-
-        # Look for the start of the packet
-        if not in_packet and prev_byte == 170 and cur_byte == 170:
+        if not in_packet and prev_byte == 0xAA and cur_byte == 0xAA:
             in_packet = True
-            packet = []
+            payload = None
             continue
 
         if in_packet:
-            if len(packet) == 0:
-                if cur_byte == 170:
+            if payload is None:
+                if cur_byte == 0xAA:
                     continue
                 packet_len = cur_byte
                 if packet_len >= MAX_PACKET_LEN:
-                    raise ValueError("Packet too long: %s" % packet_len)
+                    print("Warning: Packet too long: %s" % packet_len)
+                    continue
                 checksum_total = 0
-                packet = [cur_byte]
-            elif len(packet) - 1 == packet_len:
+                payload = []
+
+            elif len(payload) == packet_len:
                 packet_checksum = cur_byte
                 in_packet = False
-                if (~(checksum_total & 255) & 255) == packet_checksum:
-                    if packet_len > 4:
-                        yield aggregated_parser(packet)
-                    else:
-                        yield raw_parser(packet)
-                else:
+                if (~(checksum_total & 0xFF) & 0xFF) != packet_checksum:
                     print("Warning: invalid checksum")
+                    continue
+
+                if packet_len > 4:
+                    packet = aggregated_parser(payload)
+                else:
+                    packet = raw_parser(payload)
+                if packet is not None:
+                    yield packet
             else:
                 checksum_total += cur_byte
-                packet.append(cur_byte)
+                payload.append(cur_byte)
 
         # keep track of last byte to catch sync bytes
         prev_byte = cur_byte
 
 
-def raw_parser(packet: list[int]) -> Raw:
-    code_level = packet[1]
+def raw_parser(payload: list[int]) -> Optional[Raw]:
+    code_level = payload[0]
     if code_level != 0x80:
-        raise ValueError(f"raw packet with unexpected code '{code_level}`")
+        print(f"Warning: raw packet with unexpected code '{code_level}`")
+        return None
 
-    vlength = packet[2]
+    vlength = payload[1]
     if vlength != 2:
-        raise ValueError(f"raw packet with unexpected vlength '{vlength}`")
+        print(f"Waning: raw packet with unexpected vlength '{vlength}`")
+        return None
 
-    value = (packet[3] << 8) | packet[4]
+    value = (payload[2] << 8) | payload[3]
 
     # Check if the sign bit is set
     if value & 0x8000:
@@ -72,45 +76,49 @@ def raw_parser(packet: list[int]) -> Raw:
     return Raw(value=value)
 
 
-def aggregated_parser(packet: list[int]) -> Aggregated:
+def aggregated_parser(payload: list[int]) -> Optional[Aggregated]:
     quality: Optional[int] = None
     attention: Optional[int] = None
     meditation: Optional[int] = None
     eeg: list[int] = []
 
-    # The first byte in the list was packet_len, so start at i = 1
-    i = 1
-    while i < len(packet) - 1:
-        code_level = packet[i]
+    i = 0
+    while i < len(payload):
+        code_level = payload[i]
         # signal quality
         if code_level == 0x02:
-            quality = packet[i + 1]
+            quality = payload[i + 1]
             i += 2
         # attention
         elif code_level == 0x04:
-            attention = packet[i + 1]
+            attention = payload[i + 1]
             i += 2
         # meditation
         elif code_level == 0x05:
-            meditation = packet[i + 1]
+            meditation = payload[i + 1]
             i += 2
         # EEG power
         elif code_level == 0x83:
             for c in range(i + 1, i + 25, 3):
-                eeg.append(packet[c] << 16 | packet[c + 1] << 8 | packet[c + 2])
+                eeg.append(payload[c] << 16 | payload[c + 1] << 8 | payload[c + 2])
             i += 26
         # Raw Wave Value
         else:
-            raise ValueError(f"unexpected code '{code_level}'")
+            print(f"Warning: unexpected crode '{code_level}'")
+            return None
 
     if quality is None:
-        raise ValueError("quality is None")
+        print("Warning: quality is None")
+        return None
     if attention is None:
-        raise ValueError("attention is None")
+        print("Warning: attention is None")
+        return None
     if meditation is None:
-        raise ValueError("meditation is None")
+        print("Warning: meditation is None")
+        return None
     if len(eeg) != 8:
-        raise ValueError(f"invalid eeg readings: {len(eeg)}")
+        print(f"Warning: invalid eeg readings: {len(eeg)}")
+        return None
 
     return Aggregated(
         quality=quality,
